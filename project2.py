ab29be3099ba8d0b77b550b0edb095a507f0a9e5
radiantquasar-lab <jlittlestone1@gmail.com>
2025-08-27 13:43:56 -0400

    Update project2.py
    downloading, loading database into sql and one query

--- project2.py
+++ project2.py
@@ -1,17 +1,39 @@
 import sqlite3
 import pandas as pd
-connection = sqlite3.connect('company.db') #connection to the db
-cursor = connection.cursor() #object to run queries
+import requests
 
-df = pd.read_csv('test.csv')
-#print(df.info())
+##Downloading file
+url = "https://data.wa.gov/api/views/f6w7-q2d2/rows.csv?accessType=DOWNLOAD"
+filename = "test.csv"
 
-name_table = 'Companies'
+response = requests.get(url)
+with open(filename, "wb") as f:
+    f.write(response.content)
 
-columns_with_types = ", ".join([f"{col.replace(' ', '_')} TEXT" for col in df.columns]) # Eg: page_id TEXT, name TEXT, urslug TEXT, ...
-create_table_query = f"CREATE TABLE IF NOT EXISTS {name_table} ({columns_with_types})"
+connection = sqlite3.connect('test.db')
 
-cursor.execute(create_table_query)
+df = pd.read_csv('test.csv', header=0)
+##SQL Table generation & Import
+
+# table name
+name_table = 'data'
+
+#header row
+
+df.columns = [col.replace(" ","_").lower() for col in df.columns]
+
+#inserting rows
+df.to_sql(name_table, connection, if_exists='replace', index=False)
+
 connection.commit()
 connection.close()
 
+#Selecting the Top 100 fuel efficient cars
+sqlitefile = "test.db"
+conn = sqlite3.connect(sqlitefile)
+c = conn.cursor()
+f = open('selecttop100.sql','r')
+sql = f.read()
+
+df = pd.read_sql_query(sql,conn)
+df.to_csv("selecttop100.csv", index=False)
