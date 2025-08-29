d98e750801a709e3ec737788cf4816e63b2af771
radiantquasar-lab <jlittlestone1@gmail.com>
2025-08-29 11:00:45 -0400

    counties_with_highest_range_average.sql

--- selecttop100.sql
+++ selecttop100.sql
@@ -1,6 +1,4 @@
-select model_year, make, model, electric_vehicle_type, [clean_alternative_fuel_vehicle_(cafv)_eligibility], electric_utility, county
+select county, AVG(electric_range) 
 from data
-where model_year BETWEEN 2010 and 2015
-and [clean_alternative_fuel_vehicle_(cafv)_eligibility] not like '%clean%'
-order by model_year asc
-LIMIT 100
+Group by county
+order by avg(electric_range) desc
